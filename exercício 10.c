bbcd2f3e5918da511c698d898d588f207c9ba82d
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-09-05 20:18:21 -0300

    exercício 10.c

---
+++ exercício 10.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) {
+  float a, b, c, x, x1, x2, delta, equacao;
+
+  printf("De um valor para A:\n");
+  scanf("%f", &a);
+  printf("De um valor para B:\n");
+  scanf("%f", &b);
+  printf("De um valor para C:\n");
+  scanf("%f", &c);
+
+  delta = (pow(b, 2) - (4 * a *c));
+  
+  
+
+  if (delta < 0){
+    printf("Não existe raiz");
+  }
+  else if(delta == 0 ){
+    
+    x1 = ((- (b) + (sqrt(delta))) / 2 * a);
+    printf("%.2f, contem uma raiz unica", x);
+  }
+  else if (delta >= 0){
+
+    x1 = ((- (b) + (sqrt(delta))) / 2 * a); 
+    x2 = ((- (b) + (sqrt(delta))) / 2 * a); 
+    equacao = (a * (( x * x)) + (b * x) + c);
+
+    printf("possui duas raizes reais: %.2f, %.2f", x1, x2 );
+    
+  }else if (equacao == 0 ){
+    printf("Não existe equação do segundo grau");
+  }
+}
