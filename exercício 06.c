9d0c705d3c757098dc854b7c2ecb5c620649cd94
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-09-05 20:15:18 -0300

    exercício 06.c

---
+++ exercício 06.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  int numero;
+  printf("Digite um numero\n");
+  scanf("%d", &numero);
+  if ((numero % 2)== 0){
+
+   printf("O numero digitado é par");
+  }
+  
+    
+    
+}
